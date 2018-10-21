eb5eebe5b1c3a5974c5bdf83cb583ec0acea15df
Robert Chen <robertchen234@gmail.com>
2018-10-21 13:33:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
      
-programmer_hash = {:grace_hopper => {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}}
+programmer_hash = {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}
 
 end
 
